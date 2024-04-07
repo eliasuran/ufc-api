9db09790714cda86213444035310e1c84fd88c78
Elias Rissanen Uran <elura001@osloskolen.no>
2024-04-07 18:29:46 +0200

    dockerized website

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	base: '/',
+	plugins: [sveltekit()],
+	preview: {
+		port: 3000,
+		strictPort: true
+	},
+	server: {
+		port: 3000,
+		strictPort: true,
+		host: true,
+		origin: 'http://0.0.0.0:3000'
+	}
 });
