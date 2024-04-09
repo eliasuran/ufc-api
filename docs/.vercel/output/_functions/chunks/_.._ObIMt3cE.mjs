277fc3dae8030c6c22a03a7a7b7f6155933deb86
Elias Rissanen Uran <elura001@osloskolen.no>
2024-04-09 13:39:30 +0200

    removed .vercel dir

--- docs/.vercel/output/_functions/chunks/_.._ObIMt3cE.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__B-F-8gFI.mjs').then(n => n._);
-
-export { page };
--- docs/.vercel/output/_functions/chunks/index_B9wuz-Z3.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_BC0WjwcQ.mjs');
-
-export { page };
