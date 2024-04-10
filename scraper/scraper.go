f454f01cc68240d0811d6838b9b4bb56a540e4b2
Elias Rissanen Uran <elura001@osloskolen.no>
2024-04-10 11:29:01 +0200

    added for indivudual fights

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,5 +1,10 @@
 package main
 
+// hva har jeg gjort denne runden
+// - scraper kjører en gang i timen
+// - får data om spesifik fight og skriver til egne jsonFiler
+// - la til response
+
 import (
 	"encoding/json"
 	"fmt"
@@ -21,6 +26,10 @@ type Event struct {
 	Date     string
 	Time     string
 	Card     string
+}
+
+type AllEventInfo struct {
+	Events []EventInfo
 }
 
 type EventInfo struct {
@@ -45,7 +54,6 @@ type FighterData struct {
 }
 
 func main() {
-	// DONE: run this function every 1 hour (something less is okay for testing)
 	for {
 		// TODO: log the time each time it scrapes
 		scraper()
@@ -61,6 +69,8 @@ func check(error error) {
 }
 
 func scraper() {
+	fmt.Println("Starting scraper...")
+
 	c := colly.NewCollector()
 
 	events := Events{}
@@ -83,13 +93,26 @@ func scraper() {
 		// getting the url where there is more info about the fight
 		link := e.ChildAttr(".c-card-event--result__headline a", "href")
 
-		// starting new collector
+		///// starting new collector /////
 		d := colly.NewCollector()
 
+		eventInfo := EventInfo{ID: id, EventName: link}
+
 		d.OnHTML(".c-listing-fight__content-row", func(h *colly.HTMLElement) {
-			fighter1 := e.ChildText(".c-listing-fight__corner-given-name") + e.ChildText("c-listing-fight__corner-family-name")
-			fmt.Println("FIgher1:", fighter1)
+			title := h.ChildText(".c-listing-fight__class-text")
+			fight := Fight{Title: title}
+
+			eventInfo.MainCard.Fights = append(eventInfo.MainCard.Fights, fight)
+
+			content, err := json.MarshalIndent(eventInfo, "", "    ")
+			check(err)
+
+			fileName := fmt.Sprintf("./data/event_%d.json", id)
+			err = os.WriteFile(fileName, content, 0644)
+			check(err)
 		})
+
+		fmt.Println("Wrote JSON for fight: ", id)
 
 		d.Visit("https://www.ufc.com" + link)
 
@@ -108,6 +131,8 @@ func scraper() {
 	// use '/data/fights.json' when deploying or when devoloping with docker container
 	err = os.WriteFile("./data/fights.json", content, 0644)
 	check(err)
+
+	fmt.Println("Wrote JSON for schedule")
 
 	fmt.Println("Scraper ran successfully")
 }
